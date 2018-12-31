7ca8436ccf984db1f651c4bdb709bccf0243fb45
DANIELLE PARK <dpark1008@gmail.com>
2018-12-31 05:15:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,13 +18,12 @@ class Song
   end
 
   def self.genres
-    @@genres.uniq!
+    @@genres.uniq
   end
 
   def self.artists
-    @@artists.uniq!
+    @@artists.uniq
   end
 
-  def self.genre_count
 
 end
